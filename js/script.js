92ff859ec1d047368ec918daad459e973e722fc8
Md Rehan Farid <rehanfarid09@gmail.com>
2024-01-21 00:24:04 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation 
+
+var typed = new Typed('.typing', {
+    strings: ['Web Developer', 'Graphic Designer', 'Web Designer', 'Video Editor'],
+    typeSpeed: 50,
+    loop:10,
+});
